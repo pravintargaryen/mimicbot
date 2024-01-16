2b5f7fbfe7c98303cc5b734bdfd41da511cb4a1d
pravintargaryen <96950453+pravintargaryen@users.noreply.github.com>
2024-01-16 15:11:28 +0530

    first commit

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # Overview
 
-Welcome to C-AI bot which has been custom trained on C programming language data from book sources using langchain.js and built with React
+Welcome to Rectangular - your React & Angular Companion and built with React ❤️
+
+## Improvements Needed
+
+Prompting with input code
+Long Prompts
 
 ## Screenshot
+
 ![screenshot](https://cdn-langchain.netlify.app/2.png)
 
 ## Demo
+
 Demo app at [Netlify](https://c-ai.netlify.app/)
 
 ## Contributions
--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,75 @@
-import { useState} from 'react'
-
+import { useState } from "react";
+import logoImage from "./assets/rectangular.png";
 
 const App = () => {
-  const [text, setText] = useState('')
-  const [messages, setMessages] = useState([])
+  const [text, setText] = useState("");
+  const [messages, setMessages] = useState([]);
 
-    console.log('messages',messages)
+  console.log("messages", messages);
 
+  const LANGUAGE_MODEL_API_KEY = import.meta.env.VITE_MY_VARIABLE;
+  const LANGUAGE_MODEL_URL = `https://generativelanguage.googleapis.com/v1beta3/models/chat-bison-001:generateMessage?key=${LANGUAGE_MODEL_API_KEY}`;
 
-    const LANGUAGE_MODEL_API_KEY = import.meta.env.VITE_MY_VARIABLE
-    const LANGUAGE_MODEL_URL = `https://generativelanguage.googleapis.com/v1beta3/models/chat-bison-001:generateMessage?key=${LANGUAGE_MODEL_API_KEY}`
-
-    
   const getResponse = async () => {
-
     const payload = {
-      prompt: { "messages": [{ "content": text }]},
+      prompt: { messages: [{ content: text }] },
       temperature: 0.1,
       candidate_count: 1,
-  }
+    };
 
     const response = await fetch(LANGUAGE_MODEL_URL, {
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(payload),
-        method: "POST",
-    })
-      const data = await response.json()
-      console.log('data', data)
-      setMessages([...messages,
-          {
-            author: data.messages[0].content,
-            bot:  data.candidates[0].content
-          }
-      ])
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+      method: "POST",
+    });
+    const data = await response.json();
+    console.log("data", data);
+    setMessages([
+      ...messages,
+      {
+        author: data.messages[0].content,
+        bot: data.candidates[0].content,
+      },
+    ]);
+  };
 
-  
-  }
-  
-
-  console.log(text)
+  console.log(text);
 
   return (
     <div className="chat-bot">
       <div className="chat-header">
         <div className="info-container">
-            <h2>C Bot</h2>
+          <img
+            src={logoImage}
+            alt="Logo"
+            width={200}
+            height={200}
+            className="logo"
+          />
         </div>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-              <path fill="rgb(106, 13, 131)" fillOpacity="1"
-                    d="M0,224L60,218.7C120,213,240,203,360,186.7C480,171,600,149,720,154.7C840,160,960,192,1080,186.7C1200,181,1320,139,1380,117.3L1440,96L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
-          </svg>
       </div>
       <div className="feed">
-          {messages?.map((message, _index) =>
-              <div key={_index}>
-                  <div className="question bubble">{message.author}</div>
-                  <div id="response-text" className="response bubble">{message.bot}</div>
-              </div>
-
-
-          )}
-
+        {messages?.map((message, _index) => (
+          <div key={_index}>
+            <div className="question bubble">{message.author}</div>
+            <div id="response-text" className="response bubble">
+              {message.bot}
+            </div>
+          </div>
+        ))}
       </div>
-      <textarea id="text-input" value={text} onChange={e => setText(e.target.value)}/>
-      <button id="input-button" onClick={getResponse}>⇨</button>
+      <textarea
+        id="text-input"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <button id="input-button" onClick={getResponse}>
+        ⇨
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
